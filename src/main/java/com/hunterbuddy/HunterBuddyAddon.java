7d51c9ae4e1fca92444965d75a2af277bfbc71e2
Likon69 <andrei69330@gmail.com>
2026-07-16 22:54:55 +0200

    revert: remove 4-module port + chesttracker + EntityVelocityUpdate accessor
    Removes the additions from commit 3edd602 (Phase, ContainerTooltips,
    VisualRangeNotifier, ItemSearchBar + chesttracker/* + MixinAccessor) and
    reverts HunterBuddyAddon.java to the version from commit fc426aa
    (removes the new module registrations).
    
    Working tree restored to: no 4-module port, no chesttracker
    package, no new accessor, no new module registrations in
    HunterBuddyAddon.

--- src/main/java/com/hunterbuddy/HunterBuddyAddon.java
+++ src/main/java/com/hunterbuddy/HunterBuddyAddon.java
@@ -2,6 +2,13 @@ package com.hunterbuddy;
 
 
 import com.hunterbuddy.hud.DimensionCoords;
+import com.hunterbuddy.gui.themes.DarkTheme;
+import com.hunterbuddy.gui.themes.LambdaTheme;
+import com.hunterbuddy.gui.themes.MidnightTheme;
+import com.hunterbuddy.gui.themes.MonochromeTheme;
+import com.hunterbuddy.gui.themes.PhosphorTheme;
+import com.hunterbuddy.gui.themes.SnowyTheme;
+import com.hunterbuddy.gui.themes.StardustTheme;
 import com.hunterbuddy.hud.DubCounterHud;
 import com.hunterbuddy.hud.ElytraHelperHud;
 import com.hunterbuddy.hud.ElytraStatusHud;
@@ -57,6 +64,7 @@ import com.hunterbuddy.modules.tradingsystem.ExperienceTraderModule;
 import com.hunterbuddy.modules.tradingsystem.ExperienceTraderStarterModule;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.gui.GuiThemes;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
@@ -74,6 +82,15 @@ public class HunterBuddyAddon extends MeteorAddon {
     @Override
     public void onInitialize() {
         LOG.info("Initializing HunterBuddy Addon");
+
+        // Themes (ported from stardust-main)
+        GuiThemes.add(DarkTheme.INSTANCE);
+        GuiThemes.add(SnowyTheme.INSTANCE);
+        GuiThemes.add(LambdaTheme.INSTANCE);
+        GuiThemes.add(StardustTheme.INSTANCE);
+        GuiThemes.add(MidnightTheme.INSTANCE);
+        GuiThemes.add(PhosphorTheme.INSTANCE);
+        GuiThemes.add(MonochromeTheme.INSTANCE);
 
         // HUDs
         Hud.get().register(ElytraHelperHud.INFO);
